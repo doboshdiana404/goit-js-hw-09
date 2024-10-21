eabc766e861f21c680c4b548d806dc0d929d9cf6
doboshdiana404 <99892249+doboshdiana404@users.noreply.github.com>
2024-10-21 14:02:32 +0000

    Deploying to gh-pages from @ doboshdiana404/goit-js-hw-09@75997e6a782f7b00b0b00bb9b71f09ef7c233cf7 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("gallery");console.log("im");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("gallery");
 //# sourceMappingURL=1-gallery.js.map
